ff6af44af1bca69274d0eabd0f519336fc2cfebd
村岡奨貴 <muraokashoki@muraokasusumutakas-MacBook-Pro.local>
2023-07-22 11:22:34 +0900

    2日目を進めた

--- next-microcms/next.config.js
+++ next-microcms/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+  },
 }
 
 module.exports = nextConfig
+
