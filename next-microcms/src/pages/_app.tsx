c6e8a98bdb160a9643f3d27a3215e8e5d49a1b17
村岡奨貴 <muraokashoki@muraokasusumutakas-MacBook-Pro.local>
2023-08-19 14:00:36 +0900

    最終日の途中経過

--- next-microcms/src/pages/_app.tsx
+++ next-microcms/src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppPropsWithLayout } from "next/app";
+import { NextSeo } from "next-seo";
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <>
+      <NextSeo titleTemplate={"%s| Next microCMS Practice"} />
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
